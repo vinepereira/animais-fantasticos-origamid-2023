170555efc4a28b821f73d8afa4470111f9e467ab
Vinícius Pereira <vinicius.pereira23@hotmail.com>
2023-07-07 16:29:41 -0300

    tooltip e modal add

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,16 @@ import initScrollSmooth from './modules/scroll-suave.js';
 import initAnimaScroll from './modules/scroll-animacao.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
+import initModal from './modules/modal.js';
+import initTooltip from './modules/tooltip.js';
+
 
 
 initScrollSmooth();
 initAnimaScroll();
 initAccordion();
 initTabNav();
+initTabNav();
+initModal();
+initTooltip();
+
