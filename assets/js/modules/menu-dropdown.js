e1046324fdb3ec8038a7ce4f306eb9b85c6a9c90
Vinícius Pereira <vinicius.pereira23@hotmail.com>
2023-07-10 11:20:13 -0300

    menu dropdown add

---
+++ assets/js/modules/menu-dropdown.js
@@ -0,0 +1,22 @@
+import outsideClick from './outsideclick.js';
+
+export default function initDropdownMenu() {
+  const dropdownMenu = document.querySelectorAll('[data-dropdown]');
+
+  if (dropdownMenu.length) {
+    dropdownMenu.forEach(menu => {
+      ['touchstart', 'click'].forEach(userEvent => {
+        menu.addEventListener(userEvent, handleClick)
+      });
+    })
+
+    function handleClick(event) {
+      event.preventDefault();
+      this.classList.add('active');
+
+      outsideClick(this, ['touchstart', 'click'], () => {
+        this.classList.remove('active');
+      });
+    }
+  }
+}
--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,7 @@ import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabnav.js';
 import initModal from './modules/modal.js';
 import initTooltip from './modules/tooltip.js';
+import initDropdownMenu from './modules/menu-dropdown.js';
 
 
 
@@ -14,4 +15,4 @@ initTabNav();
 initTabNav();
 initModal();
 initTooltip();
-
+initDropdownMenu();
